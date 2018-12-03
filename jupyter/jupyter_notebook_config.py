9e49d76294fabcd94f757fa1d4054cd1586d796f
Jitse-Jan <jitsejan@development.jitsejan.com>
2018-12-03 17:55:50 +0000

    Updated Jupyter files

---
+++ jupyter/jupyter_notebook_config.py
@@ -0,0 +1,8 @@
+c = get_config()
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.open_browser = False
+c.NotebookApp.password = u'sha1:b3af1b4adee9:9e86cb52435cc24db0b487451c10f6d348734645'
+c.NotebookApp.port = 5006
+c.NotebookApp.allow_origin = '*' 
+c.InteractiveShell.ast_node_interactivity = "all"
+
